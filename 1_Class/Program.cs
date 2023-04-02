8bdc861b8034f4a54830a797c095f556853ab10f
Okan Candeniz <candenizokan@gmail.com>
2023-04-02 13:31:40 +0300

    personel yazdırma

--- 1_Class/Program.cs
+++ 1_Class/Program.cs
@@ -22,7 +22,55 @@ namespace _1_Class
 
              */
 
-            Personel personel = new Personel();
+            Personel personel = new Personel();//instancee:örneklem
+            personel.Id = 1;
+            personel.Ad = "okan";
+            personel.Soyad = "candeniz";
+            personel.Yas = 30;
+            personel.SaglikProblemiVarMi = false;
+
+            Personel personel1 = new Personel();//instancee:örneklem
+            personel1.Id = 2;
+            personel1.Ad = "ali";
+            personel1.Soyad = "candeniz";
+            personel1.Yas = 55;
+            personel1.SaglikProblemiVarMi = false;
+
+            Personel personel2 = new Personel();//instancee:örneklem
+            personel2.Id = 3;
+            personel2.Ad = "aynur";
+            personel2.Soyad = "candeniz";
+            personel2.Yas = 48;
+            personel2.SaglikProblemiVarMi = false;
+
+            Personel personel3 = new Personel();//instancee:örneklem
+            personel3.Id = 4;
+            personel3.Ad = "ege";
+            personel3.Soyad = "candeniz";
+            personel3.Yas = 1;
+
+            List<Personel> list = new List<Personel>();
+            list.Add(personel);
+            list.Add(personel1);
+            list.Add(personel2);
+            list.Add(personel3);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Id+" "+item.Ad + " " +item.Soyad + " " +item.Yas + " " +item.SaglikProblemiVarMi);
+            }
+
+            //yaşı 35 den küçük olanları yaz
+            foreach (var item in list)
+            {
+                if (item.Yas<=35)
+                {
+                    Console.WriteLine(item.Id + " " + item.Ad + " " + item.Soyad + " " + item.Yas + " " + item.SaglikProblemiVarMi);
+                }
+            }
+            Console.WriteLine(personel.Id);
+
+            Console.ReadLine();
         }
     }
 }
