9419b1a7327871d84d0b6f4c2f661384bc3e4b1d
Okan Candeniz <candenizokan@gmail.com>
2023-04-02 13:40:47 +0300

    create constructour

---
+++ Constructor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Constructor:yapılandırıcı-yapıcı method
+            yapıcı method: static olmayan(ileride göreceğiz), bir sınıftan bir nesne almak istediğimizde çalışan oluşturulduğunda parametreli de çalışabilen bir methoddur.
+            Void tiplidir geriye değer döndürmezler!
+            kesinlikle sınıfı ile aynı ismi taşırlar.
+            aksi söylenmediğinde DEFAULT olarak zaten arka planda oluşur ve parametresiz çalışır ancak biz CONSTRUCTOR için overload yapmaya kalktığımızda !!! DEFAULTTA PARAMETRESİZ HALİNİ de SINIF içine yazmasak overload parametresiz halini ezmiş olur
+
+            Metot olduğu için tabiki overloadlar oluşturulabilir.
+             
+             */
+        }
+    }
+}
