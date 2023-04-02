16234a0ef72672f5b9ffbc2ef96802b6ef2589ea
Okan Candeniz <candenizokan@gmail.com>
2023-04-02 12:44:13 +0300

    personel property

--- 1_Class/Models/Personel.cs
+++ 1_Class/Models/Personel.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace _1_Class.Models
 {
-    internal class Personel
+    //access modifiers: private-public-protected-internal-protect internal
+    public class Personel
     {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int Yas { get; set; }
+        public bool SaglikProblemiVarMi { get; set; }
     }
 }
