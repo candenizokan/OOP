7c43e73da47cb00fcdf66f393cfcdcef558acac5
Okan Candeniz <candenizokan@gmail.com>
2023-04-02 12:26:46 +0300

    Add project files.

---
+++ 1_Class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            CLASS- sınıf: oluşmasını istediğimiz nesnelerin kalıplarıdır. soyut haldedir. doğrudan doğruya iş yapamayız ancak instance(örnek alam-örneklem) dediğimizde ortaya çıkan nesne sayesine iş yapabilir alanlarına değer atayabiliriz.
+            
+            sınıftan INSTANCE almadıkça nesne(object) varlığından söz edilemez'
+
+            Sınıf üyeleri: field, property ve metodlar olabilir.
+            field: dışarıya kapalı alanlardır. Ancak propertyler yardımıyla içindeki değer okunabilir/atama yapılabilir.
+            Property: sınıf içinde özellik atadığımız alanlardır. GET ve SET methodları sayesinde değerleri okunabilir ve değiştirilebilinir.
+
+             */
+        }
+    }
+}
