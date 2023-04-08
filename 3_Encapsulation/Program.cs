7fd330cc4e5cab41094d9bd8e5019edcbac677e0
Okan Candeniz <candenizokan@gmail.com>
2023-04-08 10:30:32 +0300

    encapsulation oluşturma

---
+++ 3_Encapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Encapsulation:
+
+            *oopnin temel prensiplerindendşr
+            *sınıf üyesi olan propertylerin bir fieldı sarıp sarmalaması ve kendi üzerinde bulunan GET ve SET methodları sayesinde içerideki fielda kontrollü erişimin ve veri paylaşımının sağlanması olayıdır.
+            *GET ve SET metodları her zaman beraber olmak zorunda değildir.
+            *içerideki fielda erişim yalnızca public bir property üzerinden olur. Field access modifierı ise privatedir.
+            *
+            *propfull + tab tab tuşlarıyla encapsulation syntaxı oluşmuş olur
+             */
+        }
+    }
+}
