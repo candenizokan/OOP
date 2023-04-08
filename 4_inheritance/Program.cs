765167c9647e17512e4649fb41147c5c616c6e88
Okan Candeniz <candenizokan@gmail.com>
2023-04-08 12:02:23 +0300

    inheritance oluşturma

---
+++ 4_inheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+            oop'un temel prensipleri
+
+            **inheritance:örnek almak, ilgili sınıfta bir nesne alma işlemidir. inheritance: kalıtım, kod aktarımı. kavramlar karışmasın
+            *
+            *ortak özellik, yeteneklerin toplandığı bir ortak sınıfın diğer sınıflara kalıtım vererek kendi içindeki tüm özellikleri ilgili sınıfa aktarması, sınıf türetmesi işlemidir.
+            *kod tekrarı önlenir, yönetim kolayaşır
+            *özellikleri düşünürken ilgili özellik, yeteknk gerçekten düşünülen sınıftan varsa aktarıma müsade edilmelidirç.
+            *unutmayalı=> özelliğe değer atanamamsı ile özelliğin olması farklı şeyle
+             */
+        }
+    }
+}
