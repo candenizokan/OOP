815363bbefc132aa703788b1c6c8c4492da94a5d
Okan Candeniz <candenizokan@gmail.com>
2023-04-09 11:54:21 +0300

    Iphone

---
+++ 6_Polymorphism/Models/Iphone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Polymorphism.Models
+{
+    internal class Iphone:Phone
+    {
+        public override void CallSound()
+        {
+            // CallSound() methodu Phone sınıfında başka şekilde Iphone içinde farklı davranışlar sergiliyor. Buna polymorphism diyoruz.
+
+            using (SoundPlayer sp = new SoundPlayer())
+            {
+                sp.SoundLocation = @"..\..\SesDosyasi\phonering.wav";
+                sp.PlaySync();
+            }
+        }
+    }
+}
