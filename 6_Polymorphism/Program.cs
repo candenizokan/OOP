08816fa63ceb43ec99acc7b0ce1391895ded3aa6
Okan Candeniz <candenizokan@gmail.com>
2023-04-09 11:37:18 +0300

    ses dosyasi eklendi

---
+++ 6_Polymorphism/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             oop un temel premsiplerindendir. birbirinden kalıtım alan sınıfların ortak bir kökenden geldiği taktirde birbirinin yerine geçebilmesi yada sınıflar içinde bir metodun inherit edildiği (kalıtım aldığı) yerde farklı şekilde içinin doldurularak kullanılnası yani metodun farklı davranışlar sergilemesi olarak düşünebiliriz.
+
+             */
+        }
+    }
+}
