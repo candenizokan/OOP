a5d208faa019ee3d5bfcc4bdfe6323f445de2ee9
Okan Candeniz <candenizokan@gmail.com>
2023-04-09 13:47:56 +0300

    yardimci

---
+++ 8_Static/Yardimci.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Static
+{
+    internal static class Yardimci
+    {
+
+        public static string Duzenle(this string word)
+        {
+            word = word.ToLower()
+                .Replace('ş', 's')
+                .Replace('ı', 'i')
+                .Replace('ğ', 'g');
+
+            return word;
+        }
+    }
+}
