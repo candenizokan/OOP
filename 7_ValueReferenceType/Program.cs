4030aff93f2c588816fc232a036394fe99e85ee3
Okan Candeniz <candenizokan@gmail.com>
2023-04-09 12:27:56 +0300

    yorum

---
+++ 7_ValueReferenceType/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_ValueReferenceType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+             ASP.Net core framework mantığı gereği uygulamada bazı nesneler RAM'in STACK kısmında bazı nesneler RAM'in  HEAP kısmında tutulur
+
+            STACK=> value type(değer tipler) + reference typların adreslerini barındırır.
+            HEAP=> referans tiplerin nesneleri yaşa
+
+            * heap kısmındaki bir nesne birden fazla adres taafından gösterilir ANCAK bir adres bir nesneyi gösterir.
+            * 
+            * VALUE TYPE kopyalanabilir ANCAK REFERENCE TYPE değer taşıma işlemi yapar.
+            * 
+            * Bir değerdin NULL olması HEAP de karşılığının olmaması demektir. Bu yüzden reference tipler null olabilir ama değer tipler null olamaz.
+            * 
+            * VALUE TYPE => tüm veriyi kendi içinde tutar. int-sbye-long-decimal-struct-enum-...
+            * 
+            * REFERENCE TYPE => adresi stack de kendi heapte bulunur. string-class-delegate
+            * 
+            * STRIN BU KONUNUN İSTİSNASIDIR. DİKKAT EDİLMELİDİR:
+             */
+        }
+    }
+}
