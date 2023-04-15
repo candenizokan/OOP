50ef8a491777b808ec31f4234d13954eb6821c58
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 11:20:11 +0300

    departman

--- 10_Enum/Personel.cs
+++ 10_Enum/Personel.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace _10_Enum
 {
-
+    //enumı sınıfın dışında namespace altında tanımlıyabiliyorum ama önermem
+    public enum Departman
+    {
+        Yazilim,
+        Muhasebe,
+        İK
+    }
     internal class Personel
     {
         public string Ad { get; set; }
         public string Soyad { get; set; }
         public KanGrup kanGrup { get; set; }
+
+        public Departman DepartmanBilgisi { get; set; }
     }
 }
