4f05ec98d86d07ff4fefac351d15fcd15cc2c486
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 13:33:12 +0300

    delega main

--- 12_Delegate/Program.cs
+++ 12_Delegate/Program.cs
@@ -8,6 +8,7 @@ namespace _12_Delegate
 {
     internal class Program
     {
+        public delegate void Logger(string message);
         static void Main(string[] args)
         {
             /*
@@ -22,6 +23,37 @@ namespace _12_Delegate
             * unutmayalım => delegate lerde birer sınıftır (value type ve referans tiplerde geçmişti)
              
              */
+
+            //2. INTANCE-ORNEKLEM
+
+            void SmsLogger(string message)
+            {
+                Console.WriteLine("SmsLogger"+message);
+            }
+
+            void MailLogger(string message)
+            {
+                Console.WriteLine("MailLogger"+message);
+            }
+
+            void XmlLogger(string message)
+            {
+                Console.WriteLine("XmlLogger"+message);
+            }
+
+            Logger logger = new Logger(MailLogger);
+            logger += SmsLogger;
+            logger += XmlLogger;
+
+            //3.Invoke - çağrı
+
+            logger.Invoke("merhabalar beklediğiniz üründe indirim var !");
+
+            Console.ReadLine();
+
+
+
+
         }
     }
 }
