37cb1c9fd7766dc55ad56d13a0cbf1764a1da9ff
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 11:18:37 +0300

    pesonelde kangrup enum tipinde

---
+++ 10_Enum/Personel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Enum
+{
+
+    internal class Personel
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public KanGrup kanGrup { get; set; }
+    }
+}
