6b888c8488cf0f60830c7f2952afead17eefa77a
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 12:39:34 +0300

    sealed

--- 11_Sealed/Models/Book.cs
+++ 11_Sealed/Models/Book.cs
@@ -11,6 +11,15 @@ namespace _11_Sealed.Models
         public int Id { get; set; }
         public string Detail { get; set; }
 
+        public override void GetRead()
+        {
+            base.GetRead();
+        }
 
+        //SEALED: keyword. kendinden nesne türetilebilir ancak kalıtım vermez. ABSTRACT kelimesinin tam tersi mantığında çalışır. Abstractlar kalıtım verebilir, nesne alınamaz. sealed newlerim kalıtım aldıramam. abs newleyemem kalırım veririm
+        public sealed override void GetUser()
+        {
+            base.GetUser();
+        }
     }
 }
