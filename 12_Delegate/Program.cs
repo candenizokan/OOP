b7e5832e95547f22d8f7e4107f6588db2f2f5c7d
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 13:22:45 +0300

    delega

---
+++ 12_Delegate/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_Delegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Delegate 
+            * bir olay gerçekleştiği zaman birden fazla fonksiyonu çağırmak istersek delegate oluşturup kullanırız.
+            * bir delegate oluşturmak için aşamalar
+            * tanımlama - declare
+            * orneklem - instance
+            * çağrıda bulunma - invoke
+            * 
+            * temsilci olarak da adlandırılan bu yapı projeyi ayakta tutan yapıları yormadan, onların işlemlerini üzerine alan tiptir
+            * unutmayalım => delegate lerde birer sınıftır (value type ve referans tiplerde geçmişti)
+             
+             */
+        }
+    }
+}
