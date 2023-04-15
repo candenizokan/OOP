f220c6a5672dacda4215329c4a6db4badc1d19f1
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 10:20:58 +0300

    create interface

---
+++ 9_Interface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             
+             Interface
+            * Interfacelerin adlandırılması I harfi ile başlar ve pascal case ile yazılır.
+            * Interfaceler katı kontrat mantığındadır., içindeki metod ve proplar implemantasyon yapıldığı yerde rek tek açıklanmak ve çalışmak zorundadır.
+            * Interfaceler hem prop hemde metod barındırailir.
+            * Interface içindeki metodların gövdeleri(süslü parantezlerin içi yani çalışma şekilleri) yeni açıklamaları yoktur, implemantasyon yapıldığı yerde zaten açıklanır.
+            * 
+            * NOT =Z Abstract sınıfın içindeki abstract metod ile interface içindeki metodların gövdeleri yoktur çünkü gittikleri yerde açıklanacaktır.. multiinheritance da bir sınıf yalnızca! bir sınıftan kalıtım alabildiğinde ikisininde varlığı ayrı ayrı önemlidir. Tek bir sınıf bizim için yeterli olmadığında interfacelerden destek alabiliriz.
+            
+             */
+        }
+    }
+}
