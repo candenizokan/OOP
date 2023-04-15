a7e199464322036ad4e527d3fabc2cfe857fb4b6
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 12:57:13 +0300

    .

--- 11_Sealed/Models/LastBook.cs
+++ 11_Sealed/Models/LastBook.cs
@@ -8,5 +8,13 @@ namespace _11_Sealed.Models
 {
     internal class LastBook:Book
     {
+        public override void GetRead()
+        {
+            base.GetRead();
+        }
+
+        //NOT LASTBOOK un türetildiği sınıfta yeni BOOK sınıfındaki GetUser() metodu SEALED işaretlendiği için o metodun görülebileceği son yer artık orasıdır. Aktarımı yapılmaz. Haliyle LastBook sınıfı içinde görülmez.
+
+        //getuser i book'ta sealed yaptığımdan kalıtımı kestim. burada override edemiyorum
     }
 }
