afab50db4ad2db8166e1b69ebc36cec54a34b1d8
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 13:55:02 +0300

    event-araba

---
+++ 13_Event/Araba.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Event
+{
+    internal class Araba
+    {
+        public Araba(string marka,int hiz)
+        {
+            Marka = marka;
+            Hiz = hiz;
+        }
+
+        private string _marka;
+
+        public string Marka
+        {
+            get { return _marka; }
+            set { _marka = value; }
+        }
+
+        private int _hiz;
+
+        public int Hiz
+        {
+            get { return _hiz; }
+            set { _hiz = value; }
+        }
+
+
+    }
+}
---
+++ 13_Event/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             
+            Event-olay
+            * örnek olarak kullanıcının fareyle tıklanmış/linke butona tıklaması yada kod akışında beklediğinmiz yada olabileceğini düşündüğümüz bir olayın olması durumudur.
+            * 
+            * olay ilk önce eventHandler yani olay yakalıyıcı mekanizması tarafından yakalanır ve devamında mekanizma delegate ile akışa devam eder ve yapılacakları delegate yapar
+            * 
+            * delegate oluşturma işlemleri burada da gerçekleşir
+            * 
+            * dikkat edilmesi gereken nokta =>olaylar gerçekleştiğinde yakalayıcı mekanizma adı ile delegenin imzasının aynı olması gerektiğidir ki kimle kimin çalıştığı anlaşılsın.
+            * 
+
+             */
+        }
+    }
+}
