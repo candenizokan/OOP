b40bfb0c99e168339030367b35951f109f57f8c9
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 12:34:28 +0300

    sealed

---
+++ 11_Sealed/Models/BaseBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Sealed.Models
+{
+    internal class BaseBook
+    {
+        public DateTime CreateDate { get; set; }
+        public string Name { get; set; }
+
+        public virtual void GetUser()
+        {
+            //çalışacak kodlar
+        }
+
+        public virtual void GetRead()
+        {
+            //çalışılacak kod blok
+        }
+    }
+}
