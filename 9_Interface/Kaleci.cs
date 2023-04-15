553e1cd3b223e9895c6adc882883eee17f073ec2
Okan Candeniz <candenizokan@gmail.com>
2023-04-15 10:30:42 +0300

    kaleciye implement

--- 9_Interface/Kaleci.cs
+++ 9_Interface/Kaleci.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace _9_Interface
 {
-    internal class Kaleci
+    internal class Kaleci : IFutbolcu
     {
+        public string Ad { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Soyad { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool MilliTakimOyuncusuMu { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public void SutCek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TopKurtar()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
